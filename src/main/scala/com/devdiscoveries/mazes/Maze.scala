915359da5e1c8785d2438b3ba4e0a9b4915ea56b
Hugo Valk <hugo.valk@gmail.com>
2016-10-25 07:53:48 +0200

    Add possibility to draw a maze in an image.

--- src/main/scala/com/devdiscoveries/mazes/Maze.scala
+++ src/main/scala/com/devdiscoveries/mazes/Maze.scala
@@ -1,5 +1,8 @@
 package com.devdiscoveries.mazes
 
+import java.awt.{BasicStroke, Color}
+import java.awt.image.BufferedImage
+
 import com.devdiscoveries.mazes.algorithms.MazeAlgorithm
 
 /**
@@ -38,4 +41,29 @@ class Maze(val nrows: Int, val ncols: Int) {
       println("")
     }
   }
+
+  def renderMaze(cellWidth: Int): BufferedImage = {
+    val image = new BufferedImage(cellWidth * (ncols + 2), cellWidth * (nrows + 2), BufferedImage.TYPE_INT_ARGB)
+    val graphics = image.createGraphics()
+    graphics.setColor(Color.white)
+    graphics.fillRect(0,0,cellWidth * (ncols + 2), cellWidth * (nrows + 2))
+    graphics.setStroke(new BasicStroke(2))
+    graphics.setColor(Color.black)
+
+    for {
+      row <- rows.zipWithIndex
+      cell <- row._1.zipWithIndex
+    } yield {
+      val x_offset = (cell._2 + 1) * cellWidth
+      val y_offset = (row._2 + 1) * cellWidth
+      if (northOf(cell._1).isEmpty) graphics.drawLine(x_offset,y_offset,x_offset + cellWidth,y_offset)
+      if (westOf(cell._1).isEmpty) graphics.drawLine(x_offset,y_offset,x_offset,y_offset + cellWidth)
+      if (!eastOf(cell._1).exists(linked(cell._1, _))) graphics.drawLine(x_offset + cellWidth,y_offset,x_offset + cellWidth,y_offset + cellWidth)
+      if (!southOf(cell._1).exists(linked(cell._1, _))) graphics.drawLine(x_offset,y_offset + cellWidth,x_offset + cellWidth,y_offset + cellWidth)
+    }
+
+    image
+  }
+
+  def save = ???
 }
--- src/main/scala/com/devdiscoveries/mazes/mazes.sc
+++ src/main/scala/com/devdiscoveries/mazes/mazes.sc
@@ -1,8 +1,16 @@
+import java.io.File
+import javax.imageio.ImageIO
+
 import com.devdiscoveries.mazes._
 import com.devdiscoveries.mazes.algorithms.{AldousBroder, BinaryTree, Sidewinder, Wilsons}
 
-val maze = new Maze(10, 10)
+val maze = new Maze(50, 50)
   with SquareGrid
-  with Sidewinder
+  with AldousBroder
 
-maze.printMaze
+//maze.printMaze
+
+val image = maze.renderMaze(50)
+val path = "/home/hugovalk/projects/mazes/test.png"
+val file = new File(path)
+ImageIO.write(image, "PNG", file)
